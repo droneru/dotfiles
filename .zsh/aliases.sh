135d947475757b4650aa183f4ec6a89c5a88b1d9
Drone <drone@drone.ru>
2012-06-29 14:02:09 +0400

    zsh

--- .zsh/aliases.sh
+++ .zsh/aliases.sh
@@ -5,7 +5,7 @@ alias -s {avi,mpeg,mpg,mov,m2v,flv}=mplayer
 alias -s txt=$EDITOR
 alias -s py=python
 alias -s {ogg,mp3,wav,wma}=mplayer
-alias -s {xls,doc,rtf,ppt,odt,sxw}=open-office
+alias -s {xls,doc,rtf,ppt,odt,sxw}=libreoffice
 alias -s {png,gif,jpg,jpeg,xpm}=feh
 alias -s {pdf,djvu}=evince
 
--- .zsh/constants.sh
+++ .zsh/constants.sh
@@ -6,3 +6,5 @@ HISTSIZE=1000
 SAVEHIST=1000 
 
 export EDIOR=vim
+export LD_LIBRARY_PATH="/opt/ibm/lotus/notes:/usr/lib32"
+

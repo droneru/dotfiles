278c3cd84b617e0356f582229c64723c408eb44e
Andrey Pavlenko <drone@drone.ru>
2013-01-16 14:10:46 +0400

    zsh aliases for git

--- .zsh/aliases.sh
+++ .zsh/aliases.sh
@@ -5,11 +5,13 @@ alias -s {avi,mpeg,mpg,mov,m2v,flv}=mplayer
 alias -s txt=$EDITOR
 alias -s py=python
 alias -s {ogg,mp3,wav,wma}=mplayer
-alias -s {xls,doc,rtf,ppt,odt,sxw}=libreoffice
+alias -s {xls,doc,docx,rtf,ppt,odt,sxw}=libreoffice
 alias -s {png,gif,jpg,jpeg,xpm}=feh
 alias -s {pdf,djvu}=evince
+alias -s {html,htm}=firefox
 
 alias ls='ls --color=auto'
+alias d='df -hHlT'
 alias grep='grep --colour=auto'
 alias ll='ls -l'
 alias la='ls -a'
@@ -23,3 +25,10 @@ alias 64='ACCEPT_KEYWORDS="~amd64"'
 alias mkc='gvim /etc/make.conf'
 alias ss='cd ~/src/workspaceSpo/spoSystem/'
 
+#git
+alias go='git checkout'
+alias gs='git status '
+alias ga='git add '
+alias gb='git branch '
+alias gc='git commit'
+alias gd='git diff'

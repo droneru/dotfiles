2ff718f0a0b097bdda54af65b35f2bc1d8077b91
andreypav <andreypav@yandex-team.ru>
2026-04-16 17:22:47 +0300

    add wezterm config and delete not active bin files

--- wezterm.lua
+++ wezterm.lua
@@ -11,5 +11,32 @@ config.font_size = 14
 config.color_scheme = "nord"
 config.font = wezterm.font("JetBrains Mono")
 
+-- Keybindings
+config.keys = {
+  -- cmd-enter / cmd-i: split pane vertically (left/right)
+  {
+    key = "Enter",
+    mods = "CMD",
+    action = wezterm.action.SplitPane { direction = "Down" },
+  },
+  {
+    key = "i",
+    mods = "CMD",
+    action = wezterm.action.SplitPane { direction = "Right" },
+  },
+  -- cmd-j: activate next pane
+  {
+    key = "j",
+    mods = "CMD",
+    action = wezterm.action.ActivatePaneDirection "Next",
+  },
+  -- cmd-l: toggle zoom for current pane
+  {
+    key = "l",
+    mods = "CMD",
+    action = wezterm.action.TogglePaneZoomState,
+  },
+}
+
 -- Finally, return the configuration to wezterm:
 return config

f0b5a41a85617f2b324866eff11a2a9c95116fb2
andreypav <andreypav@yandex-team.ru>
2026-04-17 10:10:38 +0300

    убрал ненужные коменты

--- wezterm.lua
+++ wezterm.lua
@@ -13,29 +13,26 @@ config.font = wezterm.font("JetBrains Mono")
 
 -- Keybindings
 config.keys = {
-  -- cmd-enter / cmd-i: split pane vertically (left/right)
-  {
-    key = "Enter",
-    mods = "CMD",
-    action = wezterm.action.SplitPane { direction = "Down" },
-  },
-  {
-    key = "i",
-    mods = "CMD",
-    action = wezterm.action.SplitPane { direction = "Right" },
-  },
-  -- cmd-j: activate next pane
-  {
-    key = "j",
-    mods = "CMD",
-    action = wezterm.action.ActivatePaneDirection "Next",
-  },
-  -- cmd-l: toggle zoom for current pane
-  {
-    key = "l",
-    mods = "CMD",
-    action = wezterm.action.TogglePaneZoomState,
-  },
+	{
+		key = "Enter",
+		mods = "CMD",
+		action = wezterm.action.SplitPane({ direction = "Down" }),
+	},
+	{
+		key = "i",
+		mods = "CMD",
+		action = wezterm.action.SplitPane({ direction = "Right" }),
+	},
+	{
+		key = "j",
+		mods = "CMD",
+		action = wezterm.action.ActivatePaneDirection("Next"),
+	},
+	{
+		key = "l",
+		mods = "CMD",
+		action = wezterm.action.TogglePaneZoomState,
+	},
 }
 
 -- Finally, return the configuration to wezterm:

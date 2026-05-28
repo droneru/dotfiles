4d08d6574ea2ad1a30a6b4822ced3c5655e59d62
Andrey Pavlenko <drone@drone.ru>
2026-05-28 10:59:22 +0300

    красивости wezterm

--- wezterm.lua
+++ wezterm.lua
@@ -7,9 +7,15 @@ local config = wezterm.config_builder()
 -- This is where you actually apply your config choices.
 
 -- or, changing the font size and color scheme.
-config.font_size = 14
+config.font_size = 15
 config.color_scheme = "nord"
 config.font = wezterm.font("JetBrains Mono")
+config.window_decorations = "RESIZE"
+
+-- tab bar
+config.hide_tab_bar_if_only_one_tab = false
+config.tab_bar_at_bottom = true
+config.use_fancy_tab_bar = false
 
 -- Keybindings
 config.keys = {
